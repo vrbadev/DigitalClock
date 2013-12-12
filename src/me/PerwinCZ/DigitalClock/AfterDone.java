fdc3c888641835c5a0f46dc33bfa11c0671b1f6e
PerwinCZ <vrba.vojtech@gmail.com>
2013-12-12 15:46:24 +0100

    DigitalClock v1.7

--- src/me/PerwinCZ/DigitalClock/AfterDone.java
+++
@@ -1,9 +0,0 @@
-package me.PerwinCZ.DigitalClock;
-
-public class AfterDone implements Runnable {
-	public void run() {
-		Main.i.getClocks();
-		Main.i.console.info("[DigitalClock] Loaded "+ Main.i.clocks.size() +" clock(s).");
-		Main.i.runTasks();
-	}
-}
